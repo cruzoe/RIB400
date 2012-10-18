f0136fc9732f71992f21dab75ed27a87d5b9eae1
cruzoe <pit4@mail.ru>
2012-10-18 10:11:15 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rib400::Application.config.secret_token = '9796a3432dec053fa67e6504a199b15099b2a1badd3bc521bcf3efe0983dde9c18db08e7fa263b26e1c1f5b598d4a328a857172341b22594193e87b72bd183a9'
